80b82db50b9a81f9670e38e60ad217fd8ffd3db0
olepak <oleksandr.lepak@gmail.com>
2023-08-19 15:11:48 +0300

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let output = "";
+	let error = "ERROR";
+	if (num < 0) {
+		return error;
+	}
+	if (num == 0) {
+		return output;
+	}
+	for (let i = 0; i < num; i++) {
+		output += string;
+	}
+	return output;
 };
 
 // Do not edit below this line
