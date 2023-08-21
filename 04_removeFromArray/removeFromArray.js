02f37fbbfb7ae42690da53bab33e3535a93b31d2
olepak <oleksandr.lepak@gmail.com>
2023-08-21 17:26:38 +0300

    04 test1 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, value1) {
+	let output = array.filter((element) => {
+		return element !== value1;
+	});
+	return output;
 };
 
 // Do not edit below this line
