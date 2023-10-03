d00535491145e88549be1ea079735d9c62aa3061
olepak <oleksandr.lepak@gmail.com>
2023-10-03 07:21:54 +0300

    start 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+	let sum = 0;
+	if (end > start) {
+		for (let j = end; j >= start; j--) {
+			sum += j;
+		}
+	} else
+		for (let i = start; i <= end; i++) {
+			sum += i;
+		}
+	return sum;
 };
 
 // Do not edit below this line
