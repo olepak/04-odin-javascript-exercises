b00d201b7780c1d6770b5b6de02a4617285d3d42
olepak <oleksandr.lepak@gmail.com>
2023-10-04 06:50:22 +0300

    done 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,23 @@
 const sumAll = function (start, end) {
 	let sum = 0;
-	if (end > start) {
-		for (let j = end; j >= start; j--) {
+	if (start > end) {
+		for (let j = start; j >= end; j--) {
 			sum += j;
 		}
+	} else if (
+		typeof start !== "number" ||
+		typeof end !== "number" ||
+		start < 0 ||
+		end < 0
+	) {
+		return "ERROR";
 	} else
 		for (let i = start; i <= end; i++) {
 			sum += i;
 		}
 	return sum;
 };
+console.log(sumAll(10, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
