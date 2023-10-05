da3a3d82263aa07bed78baa7588876a05ee84a98
olepak <oleksandr.lepak@gmail.com>
2023-10-05 09:12:49 +0300

    done 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function (F) {
-	return Math.floor((F - 32) * (5 / 9) * 10) / 10;
+	let result = (F - 32) * (5 / 9);
+	return Math.round(result * 10) / 10;
 };
 
 const convertToFahrenheit = function (C) {
-	return Math.floor((C * (9 / 5) + 32) * 10) / 10;
+	let result = C * (9 / 5) + 32;
+	return (result = Math.round(result * 10) / 10);
 };
-console.log(convertToCelsius(32));
-console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
